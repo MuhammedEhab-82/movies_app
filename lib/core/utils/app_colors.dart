6cf7ec1bc74542778092034843351966ea53d7e9
MuhammedEhab-82 <sci.MohamedEhab10668@alexu.edu.eg>
2026-08-11 00:41:07 +0300

    initialize core utilities and add colors, responsive, and styles

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color primary = Color(0xFFFFBB3B);
+  static const Color background = Color(0xFF121312);
+  static const Color gray = Color(0xFF282A28);
+  static const Color red = Color(0xFFE82626);
+  static const Color white = Color(0xFFffffff);
+}
---
+++ lib/core/utils/app_routes.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class AppRoutes {
+
+  static const String onBoarding = '/onBoarding';
+  static const String logIn = '/logIn';
+  static const String forgotPassword = '/forgotPassword';
+  static const String signUp = '/signUp';
+  static const String home = '/home';
+  static const String updateProfile = '/updateProfile';
+  static const String movieDetails = '/movieDetails';
+
+  static Map<String, WidgetBuilder> routes = {
+    // onBoarding: (context) => const OnboardingScreen(),
+    // logIn: (context) => const LoginScreen(),
+    // signUp: (context) => const SignUpScreen(),
+    // home: (context) => const HomeScreen(),
+    // updateProfile: (context) => const UpdateProfileScreen(),
+    // forgotPassword: (context) => const ForgotPasswordScreen(),
+    // movieDetails: (context) => const MovieDetailsScreen(),
+  };
+}
