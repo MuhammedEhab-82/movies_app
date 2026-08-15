929a2699c37642e6f32c898fa017e663dea7dfb9
MohanedElsaead <mohanedalsaead@gmail.com>
2026-08-15 21:13:32 +0300

    + Added Images and Icons + Added Paths to pubspec.yaml + Added AppAssets

--- lib/core/utils/app_assets.dart
+++ lib/core/utils/app_assets.dart
@@ -1,1 +1,67 @@
-class AppAssets {}
+class AppAssets {
+//###################################################### IMAGES ####################################################################
+static const String A_AppLogo="assets/images/AppLogo.png";
+static const String A_AvailableNow="assets/images/AvailableNow.png";
+static const String A_Cast="assets/images/Cast.png";
+static const String A_Empty="assets/images/Empty.png";
+static const String A_ForgetPasswordVector="assets/images/ForgetPasswordVector.png";
+static const String A_MovieCard="assets/images/MovieCard.png";
+static const String A_PlayButton="assets/images/PlayButton.png";
+static const String A_RouteLogo="assets/images/RouteLogo.png";
+static const String A_Screenshots1="assets/images/Screenshots1.png";
+static const String A_Screenshots2="assets/images/Screenshots2.png";
+static const String A_Screenshots3="assets/images/Screenshots3.png";
+static const String A_WatchNow="assets/images/WatchNow.png";
+//########################################################################
+static const String A_Profile01="assets/images/ProfileAvatars/Profile01.png";
+static const String A_Profile02="assets/images/ProfileAvatars/Profile02.png";
+static const String A_Profile03="assets/images/ProfileAvatars/Profile03.png";
+static const String A_Profile04="assets/images/ProfileAvatars/Profile04.png";
+static const String A_Profile05="assets/images/ProfileAvatars/Profile05.png";
+static const String A_Profile06="assets/images/ProfileAvatars/Profile06.png";
+static const String A_Profile07="assets/images/ProfileAvatars/Profile07.png";
+static const String A_Profile08="assets/images/ProfileAvatars/Profile08.png";
+static const String A_Profile09="assets/images/ProfileAvatars/Profile09.png";
+//########################################################################
+static const String A_MoviesPosters="assets/images/OnBoarding/MoviesPosters.png";
+static const String A_OnBoarding1="assets/images/OnBoarding/OnBoarding1.png";
+static const String A_OnBoarding2="assets/images/OnBoarding/OnBoarding2.png";
+static const String A_OnBoarding3="assets/images/OnBoarding/OnBoarding3.png";
+static const String A_OnBoarding4="assets/images/OnBoarding/OnBoarding4.png";
+static const String A_OnBoarding5="assets/images/OnBoarding/OnBoarding5.png";
+//########################################################################
+static const String A_MoviePoster1="assets/images/MoviePosters/MoviePoster1.png";
+static const String A_MoviePoster2="assets/images/MoviePosters/MoviePoster2.png";
+static const String A_MoviePoster3="assets/images/MoviePosters/MoviePoster3.png";
+static const String A_MoviePoster4="assets/images/MoviePosters/MoviePoster4.png";
+static const String A_MoviePoster5="assets/images/MoviePosters/MoviePoster5.png";
+static const String A_MoviePoster6="assets/images/MoviePosters/MoviePoster6.png";
+//###################################################### ICONS ###################################################################
+static const String I_ArrowBack="assets/icons/ArrowBack.png";
+static const String I_Back="assets/icons/Back.png";
+static const String I_Bookmark="assets/icons/Bookmark.png";
+static const String I_EG="assets/icons/EG.png";
+static const String I_Email="assets/icons/Email.png";
+static const String I_Exit="assets/icons/Exit.png";
+static const String I_Explore="assets/icons/Explore.png";
+static const String I_ExploreSolid="assets/icons/ExploreSolid.png";
+static const String I_Favourite="assets/icons/Favourite.png";
+static const String I_Google="assets/icons/Google.png";
+static const String I_Hide="assets/icons/Hide.png";
+static const String I_History="assets/icons/ArrowBack.png";
+static const String I_Home="assets/icons/Home.png";
+static const String I_HomeSolid="assets/icons/HomeSolid.png";
+static const String I_LR="assets/icons/LR.png";
+static const String I_Name="assets/icons/Name.png";
+static const String I_Password="assets/icons/Password.png";
+static const String I_Person="assets/icons/Person.png";
+static const String I_Phone="assets/icons/Phone.png";
+static const String I_Profile="assets/icons/Profile.png";
+static const String I_ProfileSolid="assets/icons/ProfileSolid.png";
+static const String I_Search="assets/icons/Search.png";
+static const String I_SearchSolid="assets/icons/SearchSolid.png";
+static const String I_Star="assets/icons/Star.png";
+static const String I_Watch="assets/icons/Watch.png";
+static const String I_WatchList="assets/icons/WatchList.png";
+//###########################################################################
+}
