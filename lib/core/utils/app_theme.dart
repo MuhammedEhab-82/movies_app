b7b77ec09390c782e8912d23177e9440902f2141
dodo <dodosayed060612@gmail.com>
2026-08-15 02:18:13 +0200

    initialized app theme .

--- lib/core/utils/app_theme.dart
+++ lib/core/utils/app_theme.dart
@@ -1,1 +1,36 @@
-class AppTheme {}
+import 'package:flutter/material.dart';
+import 'package:movies_app/core/utils/app_styles.dart';
+
+import 'app_colors.dart';
+
+class AppTheme {
+  static ThemeData darkTheme = ThemeData(
+    scaffoldBackgroundColor:AppColors.background,
+    cardColor: AppColors.primary,
+    bottomSheetTheme:BottomSheetThemeData(
+      modalBackgroundColor: AppColors.gray,
+    ),
+    bottomNavigationBarTheme: BottomNavigationBarThemeData(
+      backgroundColor: AppColors.gray,
+      unselectedItemColor: AppColors.white,
+      selectedItemColor: AppColors.primary,
+      showUnselectedLabels: false,
+      showSelectedLabels: false,
+    ),
+    appBarTheme: AppBarThemeData(
+      backgroundColor: AppColors.background,
+      centerTitle: true,
+      iconTheme: IconThemeData(
+        color: AppColors.primary,
+        size: 30,
+      ),
+      titleTextStyle: AppStyles.reg16primary,
+    ),
+    tabBarTheme: TabBarThemeData(
+      labelStyle:AppStyles.bold20black ,
+      unselectedLabelStyle:AppStyles.bold20primary,
+      indicatorColor: Colors.transparent,
+      labelPadding: EdgeInsets.symmetric(vertical:2,horizontal: 4),
+    ),
+  );
+}
