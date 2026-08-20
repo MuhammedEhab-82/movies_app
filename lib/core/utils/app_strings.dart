469d6a30b6d07975feff4e493457bf5a1fb6dd19
dodo <dodosayed060612@gmail.com>
2026-08-20 03:22:19 +0200

    initialized onBoarding screen .

--- lib/core/utils/app_strings.dart
+++ lib/core/utils/app_strings.dart
@@ -1,1 +1,28 @@
-class AppStrings {}
+import 'package:flutter/material.dart';
+
+import 'app_styles.dart';
+
+class AppStrings {
+  static final Text moviesPosterTitle =Text('Find Your Next Favorite Movie Here',style: AppStyles.med36white,);
+  static final Text moviesPosterDescription = Text ('Get access to a huge library of movies \n to suit all tastes. You will surely like it.',style:  AppStyles.reg20white,);
+  static final Text title1 = Text('Discover Movies',style: AppStyles.bold24white,);
+  static final Text title2 = Text('Explore All Genres',style: AppStyles.bold24white);
+  static final Text title3 = Text('Create Watchlists',style: AppStyles.bold24white);
+  static final Text title4 =Text('Rate, Review, and Learn',style: AppStyles.bold24white) ;
+  static final Text title5 = Text('Start Watching Now',style: AppStyles.bold24white);
+  static final Text description1 = Text('Explore a vast collection of movies in all \n qualities and genres. Find your next \n favorite film with ease.',
+      style: AppStyles.reg20white);
+  static final Text description2 = Text('Discover movies from every genre, in all  \n available qualities. Find something new \n and exciting to watch every day.',
+      style: AppStyles.reg20white);
+  static final Text description3 = Text('Save movies to your watchlist to keep \n track of what you want to watch next.\n Enjoy films in various qualities and \n genres.',
+      style: AppStyles.reg20white);
+  static final Text description4 = Text("Share your thoughts on the movies  \n you 've watched. Dive deep into film \n details and help others discover great \n movies with your reviews.",
+    style: AppStyles.reg20white,);
+  static final Text description5 = Text('');
+
+    static final String back = 'Back';
+  static final String next = 'Next';
+  static final String finish = 'Finish';
+  static final String exploreNow = 'Explore Now';
+
+}
